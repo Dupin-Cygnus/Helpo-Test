d0264a6198f7f7aebb366db003507c7744a8509b
Dupin-Cygnus <57307312+Dupin-Cygnus@users.noreply.github.com>
2019-11-02 23:19:08 -0500

    first commit

---
+++ Helpo_Bot_Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Helpo_Bot_Core
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Henlo World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+"# Helpo-Test" 
