17ee5c887ba04e6b83e863881b6a2c996cf9c125
Dupin-Cygnus <57307312+Dupin-Cygnus@users.noreply.github.com>
2019-11-06 22:24:40 -0600

    Added unit testing with xUnit

---
+++ Helpo_Bot_Core.xUnit.Tests/UtilityTests.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+namespace Helpo_Bot_Core.xUnit.Tests
+{
+    public class UtilityTests
+    {
+        [Fact]
+        public void MyFirstTest()
+        {
+            const int expected = 5;
+            var actual = Utilities.MyUtility(expected);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ Helpo_Bot_Core/Utilities.cs
@@ -0,0 +1,10 @@
+namespace Helpo_Bot_Core
+{
+    public class Utilities
+    {
+        public static int MyUtility(int i)
+        {
+            return i;
+        }
+    }
+}
